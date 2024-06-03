108e2917246ec528c8660e5ca0cf4800911f867e
ajayygurjar <ajay1651@gmail.com>
2024-06-03 15:06:53 +0530

    Step:-3 how to scale

--- index.js
+++ index.js
@@ -15,6 +15,29 @@
 
 
 // Write your code below:
+// function handleFormSubmit(event){
+//     event.preventDefault();
+//     const Username=event.target.username.value;
+//     const Email=event.target.email.value;
+//     const Phone=event.target.phone.value;
+//     const userDetail={
+//       username:Username,
+//       email:Email,
+//       phone:Phone
+//     }
+//     localStorage.setItem('User Details',JSON.stringify(userDetail));
+//   }
+//   module.exports=handleFormSubmit;
+  
+
+//Step:-3 how to scale 
+//ou were able to store single user's details in the local storage. 
+//But this is not scalable. Every time you add a new user, the older user gets removed. 
+//Try to solve this problem in this task.
+
+
+
+// Write your code below:
 function handleFormSubmit(event){
     event.preventDefault();
     const Username=event.target.username.value;
@@ -25,7 +48,15 @@ function handleFormSubmit(event){
       email:Email,
       phone:Phone
     }
-    localStorage.setItem('User Details',JSON.stringify(userDetail));
+    
+    localStorage.setItem(Email,JSON.stringify(userDetail));
+    showOnScreen(userDetail);
   }
   module.exports=handleFormSubmit;
+  function showOnScreen(userDetail){
+    const parentElem=document.getElementById('listItem');
+    const childElem=document.createElement('li')
+    childElem.textContent=userDetail.username +'-'+ userDetail.email + '-'+ userDetail.phone
+    parentElem.appendChild(childElem);
+  }
   
